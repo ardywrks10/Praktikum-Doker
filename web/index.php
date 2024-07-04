50527b1339d1f8ab7d8a1a2951a9e729a3dca9ed
ardy <ardywirakusuma16@gmail.com>
2024-07-04 23:29:14 +0800

    first commit

---
+++ web/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Belajar Docker</title>
+        <link href="dist/output.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <style>
+            body {
+                font-family: 'Montserrat', sans-serif; 
+                background-image: url('img/bg.jpg'); 
+                background-size: cover;
+                background-position: center;
+            }
+        </style>
+    </head>
+    <body>
+        <header class="bg-white fixed top-0 left-0 flex items-center z-10 w-full ">
+            <div class="container">
+                <div class="flex items-center justify-center relative">
+                    <div class="px-4">
+                        <p class="block text-xl py-6 font-extrabold text-[#004AAD] lg:text-2xl">PRAKTIKUM<span class="text-slate-800"> DOCKER</span></p>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section id="home" class="dark:bg-dark pt-20 pb-10 lg:pb-20">
+            <div class="container flex flex-col items-center justify-center min-h-screen">
+                <div class="text-center">
+                    <p class="block text-xl font-extrabold text-blue-400 lg:text-xl">SISTEM<span class="text-white"> TERDISTRIBUSI</span></p>
+                    <p class="txt font-extrabold block text-white mt-3 mb-3 text-2xl lg:text-4xl">Kadek Ardy Wirakusuma</span></p>
+                    <p class="font-bold text-gray-400 mb-5 mt-5 leading-relaxed lg:text-xl">
+                        2215101012
+                    </p>
+                    <p class="txt font-extrabold block text-white mt-3 mb-3 text-2xl lg:text-4xl"><span class="">A</span></p>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
